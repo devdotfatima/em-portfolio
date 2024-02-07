e0079470d15086a846e5c26d694a32b30d98a5bd
Fatima Ali <fatimaaliforwork@gmail.com>
2024-02-07 09:40:22 +0500

    integrates prime react and creates basic layout of the website

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { PrimeReactProvider } from "primereact/api";
+
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,7 +20,9 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>{children}</body>
+			<PrimeReactProvider>
+				<body className={inter.className}>{children}</body>
+			</PrimeReactProvider>
 		</html>
 	);
 }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	images: {
+		domains: ["images.unsplash.com", "s3-us-west-2.amazonaws.com"],
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
