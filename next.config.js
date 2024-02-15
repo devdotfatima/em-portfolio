ca27f11692e067f3522cb65ea7305c3fc047dd97
Fatima Ali <fatimaaliforwork@gmail.com>
2024-02-15 18:46:05 +0500

    updates next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		domains: ["images.unsplash.com", "s3-us-west-2.amazonaws.com"],
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "**",
+			},
+		],
 	},
 };
 
